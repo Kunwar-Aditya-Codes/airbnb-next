e7e4f53c3672e3acfe70396d90ef7285ac550a19
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-04-20 09:58:38 +0530

    date picker

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,32 @@
 import Image from 'next/image';
-import React from 'react';
+import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BsGlobe2 } from 'react-icons/bs';
 import { BiMenu, BiUserCircle } from 'react-icons/bi';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { DateRangePicker } from 'react-date-range';
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.Selection.startDate);
+    setEndDate(ranges.Selection.endDate);
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'Selection',
+  };
+
   return (
-    <header className='sticky z-[50] md:grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
+    <header className='sticky z-[50] grid grid-cols-1 md:grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       {/* Left */}
-      <div className='relative flex items-center h-10 cursor-pointer  '>
+      <div className='relative flex items-center h-10 cursor-pointer   '>
         <Image
           src='https://links.papareact.com/qd3'
           layout='fill'
@@ -18,24 +36,40 @@ const Header = () => {
       </div>
 
       {/* Middle */}
-      <div className=' hidden md:inline-flex items-center md:border-2 rounded-full p-2 md:shadow-sm '>
+      <div className='mx-auto w-full mt-3 md:mt-0 md:mx-0 flex items-center border-2 rounded-full p-2 md:shadow-sm '>
         <input
           type='text'
           placeholder='Start your search'
           className='outline-none flex-grow placeholder-gray-500'
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
         <AiOutlineSearch className='bg-red-400  h-6 w-6 p-1 text-white rounded-full cursor-pointer' />
       </div>
 
       {/* Right */}
       <div className=' hidden md:inline-flex justify-end items-center space-x-4 text-gray-600'>
-        <p className='cursor-pointer hover:text-black transition ease-in-out '>Become a host</p>
+        <p className='cursor-pointer hover:text-black transition ease-in-out '>
+          Become a host
+        </p>
         <BsGlobe2 className='h-5 w-5' />
         <div className='flex border-2 space-x-2 p-2 rounded-full'>
           <BiMenu className='h-5 w-5' />
           <BiUserCircle className='h-5 w-5' />
         </div>
       </div>
+
+      {searchInput && (
+        <div className='flex flex-col  md:col-span-3 md:mx-auto '>
+          <DateRangePicker
+            className='object-fill'
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 };
